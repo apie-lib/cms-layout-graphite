48a934f145427b2f30df65f01dfd22ec16d9b32d
pjordaan <pjordaan@users.noreply.github.com>
2022-10-23 16:22:24 +0000

    Update file(s) "packages/cms-layout-graphite/." from "apie-lib/apie-lib-monorepo"

--- src/Extension/ComponentHelperExtension.php
+++ src/Extension/ComponentHelperExtension.php
@@ -15,13 +15,13 @@ class ComponentHelperExtension extends AbstractExtension
     /** @var TwigRenderer[] */
     private array $renderers = [];
 
-    public function selectComponent(TwigRenderer $renderer, ComponentInterface $component)
+    public function selectComponent(TwigRenderer $renderer, ComponentInterface $component): void
     {
         $this->renderers[] = $renderer;
         $this->componentsHandled[] = $component;
     }
 
-    public function deselectComponent(ComponentInterface $component)
+    public function deselectComponent(ComponentInterface $component): void
     {
         if (end($this->componentsHandled) !== $component) {
             throw new LogicException('Last component is not the one being deselected');
